fd7e19b24692e01e366ba0e63e148db5feeb5f30
Lani <lanikrossa@gmail.com>
2026-09-12 23:14:48 -0700

    add connect midi button

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const keyboard = document.querySelector('.keyboard');
 for (const key of keyboard.children) {
     console.log(key.dataset.note);
 }
+
+const connect_button = document.querySelector('#connect-midi-button');
+
+connect_button.addEventListener('click', async () => {
+    console.log("Connect Midi clicked!");
+});
