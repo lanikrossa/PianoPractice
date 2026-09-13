4337bf59effbc623229aff19f2794bb5c8ffb50f
Lani <lanikrossa@gmail.com>
2026-09-13 00:22:44 -0700

    cleanup

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-const keyboard = document.querySelector('.keyboard');
+const connectButton = document.querySelector('#connect-midi-button');
+connectButton.addEventListener('click', connectMIDI);
 
-for (const key of keyboard.children) {
-    console.log(key.dataset.note);
-}
-
-const connect_button = document.querySelector('#connect-midi-button');
-
-connect_button.addEventListener('click', async () => {
+async function connectMIDI() {
     console.log("Connect Midi clicked!");
     try{
         const midiAccess = await navigator.requestMIDIAccess();
         for (const input of midiAccess.inputs.values()){
-            console.log(`found: ${input.name} ${input.manufacturer} ${input.state} ${input.connection}`)
+            console.log(`found: ${input.name} ${input.manufacturer} ${input.state} ${input.connection}`);
             await input.open();
-            console.log(`Input connection: ${input.connection}`)
-            input.addEventListener("midimessage", keyinfo)
+            console.log(`Input connection: ${input.connection}`);
+            input.addEventListener("midimessage", handleMIDIMessage);
         }
     } catch (error){
         console.log("An error occurred when connecting to MIDI: ", error.message);
     }
-});
+}
 
-function keyinfo(event){
+function handleMIDIMessage(event){
     // filter out non piano-key events (I'm here messages)
     if (event.data[0] === 254)
         return;
     const [status, note, velocity] = event.data;
-    // console.log(`Status: ${status}, Note: ${note}, Velocity: ${velocity}`)
 
-    const midi_query = `[data-midi-note="${note}"]`
-    const key = document.querySelector(midi_query);
-    if (key == null)
+    const midiQuery = `[data-midi-note="${note}"]`;
+    const key = document.querySelector(midiQuery);
+    if (key === null)
         return;
 
     if (status === 144 && velocity > 0){
