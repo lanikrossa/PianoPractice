ae8d1436a09ee7d8f8027a1d9ee989fa03539faa
Lani <lanikrossa@gmail.com>
2026-09-13 14:50:58 -0700

    force user to connect first before playing.

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
+// DOM elements
 const connectButton = document.querySelector('#connect-midi-button');
+const overlay = document.querySelector('.overlay');
+const midiStatus= document.querySelector('#midiStatus');
+
+// Scale and practice state
+const cMajorNotes = [60, 62, 64, 65, 67, 69, 71, 72];
+
+let expectedNoteIndex = 0;
+
+// MIDI connection
 connectButton.addEventListener('click', connectMIDI);
 
 async function connectMIDI() {
     console.log("Connect Midi clicked!");
     try{
         const midiAccess = await navigator.requestMIDIAccess();
+        if (midiAccess.inputs.size == 0){
+            midiStatus.textContent = "No MIDI keyboard found."
+            return;
+        }
         for (const input of midiAccess.inputs.values()){
             console.log(`found: ${input.name} ${input.manufacturer} ${input.state} ${input.connection}`);
             await input.open();
             console.log(`Input connection: ${input.connection}`);
             input.addEventListener("midimessage", handleMIDIMessage);
+
         }
+        overlay.classList.add("hidden");
     } catch (error){
         console.log("An error occurred when connecting to MIDI: ", error.message);
+        midiStatus.textContent = "Failed to connect to MIDI.";
     }
 }
 
+// MIDI event handling
 function handleMIDIMessage(event){
     // filter out non piano-key events (I'm here messages)
     if (event.data[0] === 254)
@@ -36,3 +54,7 @@ function handleMIDIMessage(event){
         key.classList.remove("pressed");
     }
 }
+
+
+
+
